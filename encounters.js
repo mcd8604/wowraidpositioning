6ce724ebdea695358f2e064e64f52fe8420ee349
mcd8604 <mcd8604@gmail.com>
2020-08-21 00:27:38 -0400

    Added support for changing encounter background images

---
+++ encounters.js
@@ -0,0 +1,17 @@
+ENCOUNTER_BACKGROUNDS = {
+	"Ouro" : {
+		url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
+		position: 	"-2548px -5306px",
+		size:		"10503px 7001px"
+	},
+	"C'Thun 1" : {
+		url : 		"CThun_Positioning.png",
+		position: 	"150px 0px",
+		size:		"800px",
+	},
+	"C'Thun 2" : {
+		url : 		"https://assets.rpglogs.com/img/warcraft/maps/321-map.png",
+		position: 	"0px 0px",
+		size:		"800px"
+	}
+}
