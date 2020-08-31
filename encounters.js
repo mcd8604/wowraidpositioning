005f21c995593346d7ab85ed7c8ade0d0d40ace0
mcd8604 <mcd8604@gmail.com>
2020-08-31 19:23:24 -0400

    Added instance selection

--- encounters.js
+++ encounters.js
@@ -1,52 +1,61 @@
-AQ_40 = {
-	"The Prophet Skeram" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/320-map.png",
-		position: 	"-2040px -1349px",
-		size:		"5948px 3964px"
+INSTANCES = {
+	"Temple of Ahn'Qiraj" : {
+		"The Prophet Skeram" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/320-map.png",
+			position: 	"-2040px -1349px",
+			size:		"5948px 3964px"
+		},
+		"Silithid Royalty" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
+			position: 	"-3500px -4250px",
+			size:		"13941px 9293px",
+		},
+		"Battleguard Sartura" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
+			position: 	"-6106px -3015px",
+			size:		"15000px 10000px"
+		},
+		"Fankriss The Unyielding" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
+			position: 	"-8940px -1800px",
+			size:		"15351px 10233px"
+		},
+		"Princess Huhuran" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
+			position: 	"-5600px -4350px",
+			size:		"14246px 9497px"
+		},
+		"Viscidus" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
+			position: 	"-6769px -838px",
+			size:		"10000px 6667px"
+		},
+		"Twin Emperors" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
+			position: 	"-5651px -4341px",
+			size:		"10205px 6803px"
+		},
+		"Ouro" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
+			position: 	"-2648px -5206px",
+			size:		"10503px 7001px"
+		},
+		"C'thun 1" : {
+			url : 		"CThun_Positioning.png",
+			position: 	"150px 0px",
+			size:		"800px",
+		},
+		"C'thun 2" : {
+			url : 		"https://assets.rpglogs.com/img/warcraft/maps/321-map.png",
+			position: 	"-817px -618px",
+			size:		"2219px 1479px"
+		}
 	},
-	"Silithid Royalty" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
-		position: 	"-3500px -4250px",
-		size:		"13941px 9293px",
-	},
-	"Battleguard Sartura" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
-		position: 	"-6106px -3015px",
-		size:		"15000px 10000px"
-	},
-	"Fankriss The Unyielding" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
-		position: 	"-8940px -1800px",
-		size:		"15351px 10233px"
-	},
-	"Princess Huhuran" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
-		position: 	"-5600px -4350px",
-		size:		"14246px 9497px"
-	},
-	"Viscidus" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
-		position: 	"-6769px -838px",
-		size:		"10000px 6667px"
-	},
-	"Twin Emperors" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
-		position: 	"-5651px -4341px",
-		size:		"10205px 6803px"
-	},
-	"Ouro" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/319-map.png",
-		position: 	"-2648px -5206px",
-		size:		"10503px 7001px"
-	},
-	"C'thun 1" : {
-		url : 		"CThun_Positioning.png",
-		position: 	"150px 0px",
-		size:		"800px",
-	},
-	"C'thun 2" : {
-		url : 		"https://assets.rpglogs.com/img/warcraft/maps/321-map.png",
-		position: 	"-817px -618px",
-		size:		"2219px 1479px"
+	"Naxxramas" : {
+		"Patchwerk" : {
+			url : 	"https://vignette.wikia.nocookie.net/wowwiki/images/f/f0/InstanceMap-Naxxramas1.jpg/revision/latest?cb=20090227001924",
+			position: 	"-2648px -5206px",
+			size:		"10503px 7001px"
+		}
 	}
 }
